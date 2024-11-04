1245902e093c2e7c588875062bcfbf81b695469d
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-11-05 00:22:28 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
     extend: {
       colors: {
         primary: "#156651",
-        text: "#404040",
         neutral: {
           // DEFAULT: "#6EBE44",
+          20: "#F5F5F5",
           40: "#E0E0E0",
+          50: "#C2C2C2",
           70: "#757575",
+          90: "#404040",
         },
       },
     },
