70743947cb312fc14a322f87b8444675b4364a0f
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-11-04 08:33:00 +0100

    chore: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#156651",
+        text: "#404040",
+        neutral: {
+          // DEFAULT: "#6EBE44",
+          40: "#E0E0E0",
+          70: "#757575",
+        },
+      },
+    },
+  },
+};
